fb89c7180518490393642706f8256c3c9676d387
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2024-10-26 09:51:36 +0100

    const init modified

--- js/script.js
+++ js/script.js
@@ -1,88 +1,90 @@
-const resultElement = document.querySelector(".js-result");
+{
+  const resultElement = document.querySelector(".js-result");
 
-const welcome = () => {
-  console.log("Hello world");
-};
+  const welcome = () => {
+    console.log("Hello world");
+  };
 
-const init = () => {
-  resultElement.innerText = (0.0).toFixed(2);
-  welcome();
-};
-
-const onFormSubmit = () => {
-  const formElement = document.querySelector(".js-form");
-
-  formElement.addEventListener("submit", showResult);
-};
-
-const GBPrate = 1;
-const EURrate = 1.19;
-const USDrate = 1.3;
-const PLNrate = 5.13;
-
-const returnRate = () => {
-  const fromCurrency = document.querySelector(".js-fromCurrency");
-  const currency = fromCurrency.value;
-
-  switch (currency) {
-    case "gbp":
-      return GBPrate;
-
-    case "eur":
-      return EURrate;
-
-    case "usd":
-      return USDrate;
-
-    case "pln":
-      return PLNrate;
-  }
-};
-
-const returnRateTo = () => {
-  const toCurrency = document.querySelector(".js-toCurrency");
-
-  const finalCurrency = toCurrency.value;
-
-  switch (finalCurrency) {
-    case "gbp":
-      return GBPrate;
-
-    case "eur":
-      return EURrate;
-
-    case "usd":
-      return USDrate;
-
-    case "pln":
-      return PLNrate;
-  }
-};
-
-const calculateResult = () => {
-  const rate = returnRate();
-  const rateTo = returnRateTo();
-  const amountElement = document.querySelector(".js-amount");
-  let amount = amountElement.value;
-
-  return (amount / rate) * rateTo;
-};
-
-const showResult = (event) => {
-  event.preventDefault();
-  const result = calculateResult();
-
-  resultElement.innerText = result.toFixed(2);
-};
-
-const reset = () => {
-  const resetButton = document.querySelector(".js-resetButton");
-
-  resetButton.addEventListener("click", (event) => {
+  const init = () => {
     resultElement.innerText = (0.0).toFixed(2);
-  });
-};
+    welcome();
+    onFormSubmit();
+    reset();
+  };
 
-init();
-onFormSubmit();
-reset();
+  const onFormSubmit = () => {
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", showResult);
+  };
+
+  const GBPrate = 1;
+  const EURrate = 1.19;
+  const USDrate = 1.3;
+  const PLNrate = 5.13;
+
+  const returnRate = () => {
+    const fromCurrency = document.querySelector(".js-fromCurrency");
+    const currency = fromCurrency.value;
+
+    switch (currency) {
+      case "gbp":
+        return GBPrate;
+
+      case "eur":
+        return EURrate;
+
+      case "usd":
+        return USDrate;
+
+      case "pln":
+        return PLNrate;
+    }
+  };
+
+  const returnRateTo = () => {
+    const toCurrency = document.querySelector(".js-toCurrency");
+
+    const finalCurrency = toCurrency.value;
+
+    switch (finalCurrency) {
+      case "gbp":
+        return GBPrate;
+
+      case "eur":
+        return EURrate;
+
+      case "usd":
+        return USDrate;
+
+      case "pln":
+        return PLNrate;
+    }
+  };
+
+  const calculateResult = () => {
+    const rate = returnRate();
+    const rateTo = returnRateTo();
+    const amountElement = document.querySelector(".js-amount");
+    let amount = amountElement.value;
+
+    return (amount / rate) * rateTo;
+  };
+
+  const showResult = (event) => {
+    event.preventDefault();
+    const result = calculateResult();
+
+    resultElement.innerText = result.toFixed(2);
+  };
+
+  const reset = () => {
+    const resetButton = document.querySelector(".js-resetButton");
+
+    resetButton.addEventListener("click", (event) => {
+      resultElement.innerText = (0.0).toFixed(2);
+    });
+  };
+
+  init();
+}
